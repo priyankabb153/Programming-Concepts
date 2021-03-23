4b6e25e080141861a2f07a18a281d76a63faf816
Priyanka Bhadrappanavar <v.bhadrappanavar@f5.com>
2021-03-24 01:28:38 +0530

    recent commit

--- 0_helloworld.c
+++ 0_helloworld.c
@@ -1,4 +1,24 @@
+/**
+ * @file 0_helloworld.c
+ * @author Priyanka
+ * @brief hello program
+ * @version 0.1
+ * @date 2021-03-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include<stdio.h>
+
+/**
+ * @brief print message
+ * 
+ * @return int 
+ */
 int main(){
+    int a=10;
+
+    int sum=20;
     printf("Welcome to step-in\n");
 }
